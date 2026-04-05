907a5698b0a5e7dc22ee4bedd4eb6b959ad3501b
8haru04 <haru10.matsu18@gmail.com>
2026-04-05 19:23:30 +0900

    入力欄の右側アイコンを調整

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "ergonomics-lab-shell-v10";
-const DATA_CACHE = "ergonomics-lab-data-v10";
+const SHELL_CACHE = "ergonomics-lab-shell-v11";
+const DATA_CACHE = "ergonomics-lab-data-v11";
 const APP_SHELL = [
   "./",
   "./index.html",
