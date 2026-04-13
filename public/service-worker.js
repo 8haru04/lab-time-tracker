bfa90d90abbb270c03c60fd85a14fc9b7601bb4a
8haru04 <haru10.matsu18@gmail.com>
2026-04-13 12:30:50 +0900

    活動記録の累計表示を小さく調整

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "ergonomics-lab-shell-v20";
-const DATA_CACHE = "ergonomics-lab-data-v20";
+const SHELL_CACHE = "ergonomics-lab-shell-v21";
+const DATA_CACHE = "ergonomics-lab-data-v21";
 const APP_SHELL = [
   "./",
   "./index.html",
